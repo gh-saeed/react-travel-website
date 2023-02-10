d79ea70dbaf8bdc78ab3140cb45ead6771c95682
saeed <ghasemkhani.saeed@gmail.com>
2023-02-10 12:43:33 +0330

    update

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,10 +1,47 @@
-import React from 'react'
-import './home.css'
+import React from "react";
+import "./home.css";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+    return (
+        <section className="home">
+            <div className="setContainer container">
+				<div className="homeText">
+					<h1 className="title">
+						Plan Your Trip With Travel Dot
+					</h1>
 
-export default Home
+					<p className="subTitle">
+						Travel to your favourite city with respectful of the enviroment!
+					</p>
+
+					<button className="btn">
+						<a href="#">explore Now</a>
+					</button>
+				</div>
+
+				<div className="homeCard grid">
+					<div className="locationDiv">
+						<label htmlFor="location">Location</label>
+						<input type="text" placeholder="Dream Destination" />
+					</div>
+
+					<div className="disDiv">
+						<label htmlFor="distance">Location</label>
+						<input type="text" placeholder="11/Meters" />
+					</div>
+
+					<div className="priceDiv">
+						<label htmlFor="price">Location</label>
+						<input type="text" placeholder="$140-$500" />
+					</div>
+
+					<button className="btn" >
+						Search
+					</button>
+				</div>
+			</div>
+        </section>
+    );
+};
+
+export default Home;
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,18 +1,82 @@
-import React from 'react'
-import './navbar.css'
+import React, { useState } from "react";
+import "./navbar.css";
+import { SiYourtraveldottv } from "react-icons/si";
+import { AiFillCloseCircle } from "react-icons/ai";
+import { TbGridDots } from "react-icons/tb";
 
 const Navbar = () => {
-  return (
-    <section className='navBarSection'>
-        <div className="header">
-            <div className="logoDiv">
-                <a href="#" className="logo">
-                    <h1></h1>
-                </a>
-            </div>
-        </div>
-    </section>
-  )
-}
+    // toggle/show navbar
+    const [active, setActive] = useState("navbar");
+    const showNav = () => {
+        setActive("navBar activeNavbar");
+    };
 
-export default Navbar
+    // remove navbar
+    const removeNav = () => {
+        setActive("navBar");
+    };
+
+    return (
+        <section className="navBarSection">
+            <div className="header">
+                <div className="logoDiv">
+                    <a href="#" className="logo">
+                        <h1 className="flex">
+                            <SiYourtraveldottv className="icon" /> Dot
+                        </h1>
+                    </a>
+                </div>
+
+                <div className={active}>
+                    <ul className="navList flex">
+                        <li className="navItem">
+                            <a href="#" className="navLink">
+                                Home
+                            </a>
+                        </li>
+                        <li className="navItem">
+                            <a href="#" className="navLink">
+                                Products
+                            </a>
+                        </li>
+                        <li className="navItem">
+                            <a href="#" className="navLink">
+                                Resources
+                            </a>
+                        </li>
+                        <li className="navItem">
+                            <a href="#" className="navLink">
+                                Cantact
+                            </a>
+                        </li>
+                        <li className="navItem">
+                            <a href="#" className="navLink">
+                                Blog
+                            </a>
+                        </li>
+
+                        <div className="headerBtns flex">
+                            <button className="btn loginBtn">
+                                <a href="#">Login</a>
+                            </button>
+
+                            <button className="btn loginBtn">
+                                <a href="#">Sign Up</a>
+                            </button>
+                        </div>
+                    </ul>
+
+                    <div onClick={removeNav} className="closeNavbar">
+                        <AiFillCloseCircle className="icon" />
+                    </div>
+                </div>
+
+                <div onClick={showNav} className="toggleNavbar">
+                    <TbGridDots className="icon" />
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Navbar;
