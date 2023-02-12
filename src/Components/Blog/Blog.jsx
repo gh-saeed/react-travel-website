a3487dcdda27240af3f0dbb1e05fd04511a1229f
saeed <ghasemkhani.saeed@gmail.com>
2023-02-12 18:09:01 +0330

    update

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -3,6 +3,38 @@ import "./blog.css";
 
 import { BsArrowRightShort } from "react-icons/bs";
 
+import img from "../../Assets/images/10.jpg";
+import img2 from "../../Assets/images/11.jpg";
+import img3 from "../../Assets/images/12.jpg";
+import img4 from "../../Assets/images/13.jpg";
+
+const Posts = [
+    {
+        id: 1,
+        postImage: img,
+        title: "Beautiful Morocco, let us travel!",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, laborum rerum! Omnis minima provident aspernatur autem harum pariatur odio adipisci.",
+    },
+    {
+        id: 2,
+        postImage: img2,
+        title: "Romantic moments under Eiffel Tower",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, laborum rerum! Omnis minima provident aspernatur autem harum pariatur odio adipisci.",
+    },
+    {
+        id: 3,
+        postImage: img3,
+        title: "Let us have an adventure outside Tunisia.",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, laborum rerum! Omnis minima provident aspernatur autem harum pariatur odio adipisci.",
+    },
+    {
+        id: 4,
+        postImage: img4,
+        title: "Best Country in East Africa",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, laborum rerum! Omnis minima provident aspernatur autem harum pariatur odio adipisci.",
+    },
+];
+
 const Blog = () => {
     return (
         <section className="blog container section">
@@ -13,24 +45,28 @@ const Blog = () => {
                 </div>
 
                 <div className="mainContainer grid">
-                    <div className="singlePost grid">
-                        <div className="imgDiv">
-                            <img src="" alt="" />
-                        </div>
+                    {Posts.map((post, i) => {
+                        return (
+                            <div className="singlePost grid" key={i}>
+                                <div className="imgDiv">
+                                    <img
+                                        src={post.postImage}
+                                        alt={post.title}
+                                    />
+                                </div>
 
-                        <div className="postDetails">
-                            <h3>Title</h3>
-                            <p>
-                                Lorem ipsum dolor sit amet consectetur,
-                                adipisicing elit. Atque, sequi.
-                            </p>
-                        </div>
+                                <div className="postDetails">
+                                    <h3>{post.title}</h3>
+                                    <p>{post.desc}</p>
+                                </div>
 
-                        <a href="#" className="flex">
-                            <BsArrowRightShort className="icon" />
-                            Read More
-                        </a>
-                    </div>
+                                <a href="#" className="flex">
+                                    Read More
+                                    <BsArrowRightShort className="icon" />
+                                </a>
+                            </div>
+                        );
+                    })}
                 </div>
             </div>
         </section>
--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,10 +1,74 @@
-import React from 'react'
-import './footer.css'
+import React from "react";
+import "./footer.css";
+import { SiYourtraveldottv } from "react-icons/si";
+import { ImFacebook, ImTwitter } from "react-icons/im";
+import { AiFillInstagram } from "react-icons/ai";
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+    return (
+        <div className="footer">
+            <div className="secContainer container grid">
+                <div className="logoDiv">
+                    <div className="footerLogo">
+                        <a href="#" className="logo flex">
+                            <h1 className="flex">
+                                <SiYourtraveldottv className="icon" /> Dot
+                            </h1>
+                        </a>
+                    </div>
 
-export default Footer
+                    <div className="socials flex">
+                        <ImFacebook className="icon" />
+                        <ImTwitter className="icon" />
+                        <AiFillInstagram className="icon" />
+                    </div>
+                </div>
+
+				<div className="footerLinks">
+					<span className="linkTitle">
+						Information
+					</span>
+					<li>
+						<a href="#">Home</a>
+					</li>
+					<li>
+						<a href="#">Explore</a>
+					</li>
+
+					<li>
+						<a href="#">Travel</a>
+					</li>
+					<li>
+						<a href="#">Blog</a>
+					</li>
+				</div>
+
+				<div className="footerLinks">
+					<span className="linkTitle">
+						Contact us
+					</span>
+					<span className="phone">+999 999 9999</span>
+					<span className="email">travel@travel.com</span>
+				</div>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
+
+
+
+// 					<li>
+// 						<a href="#">Destination</a>
+// 					</li>
+// 					<li>
+// 						<a href="#">Support</a>
+// 					</li>
+
+// 					<li>
+// 						<a href="#">Travel & Conditions</a>
+// 					</li>
+// 					<li>
+// 						<a href="#">Privacy</a>
+// 					</li>
